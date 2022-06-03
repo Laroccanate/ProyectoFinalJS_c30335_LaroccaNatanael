351e4b910d7cbe95d6719b4a5c1b90b6e5000dfe
Laroccanate <laroccanate@gmail.com>
2022-06-03 19:59:04 -0400

    Proyecto Final JS

---
+++ js/datos.js
@@ -0,0 +1,65 @@
+var productos= [
+  {
+    "id": 1,
+      "nombre": "Bife Ancho",
+      "precio": 1865,
+      "destacado": true,
+      "imagen": "./img/bifeAncho.jpg"
+  },
+  {
+    "id": 2,
+      "nombre": "Ojo de Bife",
+      "precio": 4225,
+      "destacado": true,
+      "imagen": "./img/ojoDeBife.jpg"
+  },
+  {
+    "id": 3,
+      "nombre": "Bife de Costilla con Lomo",
+      "precio": 3698,
+      "destacado": true,
+      "imagen": "./img/bifeDeCostillaConLomo.jpg"
+  },
+  {
+    "id": 4,
+      "nombre": "Asado del Centro",
+      "precio": 3049,
+      "destacado": true,
+      "imagen": "./img/asadoDelCentro.jpg"
+  },
+  {
+    "id": 5,
+      "nombre": "Brochette",
+      "precio": 2351,
+      "destacado": true,
+      "imagen": "./img/brochette.jpg"
+  },
+  {
+    "id": 6,
+      "nombre": "Churrasquito de cerdo",
+      "precio": 1250,
+      "destacado": true,
+      "imagen": "./img/churrasquito.jpg"
+  },
+  {
+    "id": 7,
+      "nombre": "Costillar 4kg",
+      "precio": 13470,
+      "destacado": true,
+      "imagen": "./img/costillar.jpg"
+  },
+  {
+    "id": 8,
+      "nombre": "Chorizo",
+      "precio": 620,
+      "destacado": true,
+      "imagen": "./img/chorizo.jpg"
+  },
+  {
+    "id": 9,
+      "nombre": "Morcilla",
+      "precio": 350,
+      "destacado": false,
+      "imagen": "./img/morcilla.jpg"
+  },
+]
